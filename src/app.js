0e1db83ad153c200f66842f408299de4e3635ee0
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 06:44:34 -0500

    Database connected and express using json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
 
 const app = express();
 const port = 3000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Server on port ${port}\n💜 http://localhost:${port}`);
--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://samuel8:t4ImxkPNMXAhIrLt@users-nodejs.eyxgrp5.mongodb.net/"
+    );
+    console.log("💜 Database connected succesfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
