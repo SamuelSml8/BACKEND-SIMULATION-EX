532ab3f2215be67961ede9131ed06c16fea143e9
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 08:18:29 -0500

    Database file documentation

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Importamos mongoose
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://samuel8:t4ImxkPNMXAhIrLt@users-nodejs.eyxgrp5.mongodb.net/"
-    );
-    console.log("💜 Database connected succesfully");
+    ); // Hacemos la conexión a la base de datos
+    console.log("💜 Database connected succesfully"); // En caso de que todo salga bien, imprime por consola que la base de datos se conectó satisfactoriamente
   } catch (error) {
-    console.log(error);
+    console.log(error); // Sino, muestra el error
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; // Exportamos el modulo para utilizarlo en in archivo principal
