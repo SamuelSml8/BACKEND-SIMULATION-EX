02c31806184096aed0b2dff4ffc6988dbecc4b45
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 06:47:04 -0500

    Student Mongoose Schema created

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  identification: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Student = mongoose.model("students_simulation", studentSchema);
+
+module.exports = Student;
