fffb7e37fc2aebe39b01493ddcf663ab90df00b6
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 07:00:25 -0500

    POST method done

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -3,8 +3,26 @@ const Student = require("../models/student.model.js");
 
 const createStudent = async (req, res) => {
   try {
+    const data = req.body;
+    const newStudent = new Student(data);
+    const identification = await Student.findOne(req.identification);
+
+    if (!identification) {
+      return res.status(400).json({
+        ok: false,
+        message: "Student already exist",
+        data: null,
+      });
+    }
+
+    const savedStudent = await newStudent.save();
+    return res.status(201).json({
+      ok: true,
+      message: "Student created succesfully",
+      data: savedStudent,
+    });
   } catch (error) {
-    console.log(`Error: `, error);
+    console.log(`Error creating a Student (POST): `, error);
     res.status(500).json({
       ok: false,
       message: "Error Internal Server",
