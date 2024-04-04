edd0d43dff16d7fc11abb7ff4b3b12f04ccf41a9
Samuel Sml <veramirandasamuel6@gmail.com>
2024-04-04 08:27:07 -0500

    app main file documentation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const connectDB = require("./config/db.js");
-const studentRoutes = require("./routes/students.routes.js");
+//* Samuel Vera Miranda
+//* 1027801700
+//* Van Rossum
 
-const app = express();
-const port = 3000;
+const express = require("express"); //Importamos express
+const connectDB = require("./config/db.js"); // Traemos o importamos la función que hace la conexión a la base de datos
+const studentRoutes = require("./routes/students.routes.js"); // Importamos las rutas de los estudiantes con los métodos HTTP
 
-connectDB();
+const app = express(); // guardamos express en una constante llamada app
+const port = 3000; // El puerto lo definimos en una constante para mayor dinamismo
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+connectDB(); // Llamamos a la función que hace la conexión a la DB para conectarnos
 
-app.use("/api/students", studentRoutes);
+app.use(express.json()); // Utilizamos JSON para las request
+app.use(express.urlencoded({ extended: false })); // Utilizamos JSON para las request en el navegador
+
+app.use("/api/students", studentRoutes); // Definimos el path que tendrán todas las rutas
 
 app.listen(port, () => {
   console.log(`Server on port ${port}\n💜 http://localhost:${port}`);
-});
+}); // Levantamiento del servidor
